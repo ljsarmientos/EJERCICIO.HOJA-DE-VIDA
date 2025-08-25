b8232c14d2dae82a0f54dd4b40660921a143158c
ljsarmientos <148922290+ljsarmientos@users.noreply.github.com>
2025-08-24 22:10:26 -0500

    Add files via upload

---
+++ Controllers/HojaDeVidaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HojaDeVidaMVC.Controllers
+{
+    public class HojaDeVidaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // va a buscar Views/HojaDeVida/Index.cshtml
+        }
+    }
+}
