01639c7666e4bb4690fac37b1fa374e8fcebdb3e
ljsarmientos <148922290+ljsarmientos@users.noreply.github.com>
2025-08-24 22:10:36 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio_Hoja_de_Vida.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
